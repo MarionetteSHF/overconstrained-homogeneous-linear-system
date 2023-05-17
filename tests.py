bce4ee2495c624ce6952387e0c31b4c5c0ae2fba
Hanfu Shi <hanfu.shi2019@gmail.com>
2023-05-17 16:52:41 -0400

    update tests.py

--- tests.py
+++ tests.py
@@ -1,34 +1,35 @@
 import oh_linear_system_module as oh
 import numpy as np
 
-def run_test_case(A, b):
+def run_test_case(A_list, b):
     solver = oh.LinearSystemSolver()
-    solver.initialize(A)
-    x = solver.solve(A[0], b)
-    residual_norm = solver.compute_residual_norm(A[0], x, b)
+    solver.initialize(A_list)
+    for A in A_list:
+        x = solver.solve(A, b)
+        residual_norm = solver.compute_residual_norm(A, x, b)
 
-    print("Constraint matrix A:")
-    print(A[0])
-    print("Right-hand side vector b:")
-    print(b)
-    print("Solution x:")
-    print(x)
-    print("Residual norm:", residual_norm)
+        print("Constraint matrix A:")
+        print(A)
+        print("Right-hand side vector b:")
+        print(b)
+        print("Solution x:")
+        print(x)
+        print("Residual norm:", residual_norm)
 
-    # Test save_state and load_state
-    solver.save_state("linear_system_state.pkl")
-    loaded_solver = oh.LinearSystemSolver()
-    loaded_solver.load_state("linear_system_state.pkl")
-    loaded_x = loaded_solver.solve(A[0],b)
-    loaded_residual_norm = loaded_solver.compute_residual_norm(A[0], loaded_x, b)
-    print("Loaded solution x:")
-    print(loaded_x)
-    print("Loaded residual norm:", loaded_residual_norm)
+        # Test save_state and load_state
+        solver.save_state("linear_system_state.pkl")
+        loaded_solver = oh.LinearSystemSolver()
+        loaded_solver.load_state("linear_system_state.pkl")
+        loaded_x = loaded_solver.solve(A,b)
+        loaded_residual_norm = loaded_solver.compute_residual_norm(A, loaded_x, b)
+        print("Loaded solution x:")
+        print(loaded_x)
+        print("Loaded residual norm:", loaded_residual_norm)
 
-    # Compare original and loaded solution and residual norm
-    np.testing.assert_allclose(x, loaded_x, rtol=1e-6, atol=1e-6)
-    assert np.isclose(residual_norm, loaded_residual_norm, rtol=1e-6, atol=1e-6)
-    print("Original and loaded solutions and residual norms match.")
+        # Compare original and loaded solution and residual norm
+        np.testing.assert_allclose(x, loaded_x, rtol=1e-6, atol=1e-6)
+        assert np.isclose(residual_norm, loaded_residual_norm, rtol=1e-6, atol=1e-6)
+        print("Original and loaded solutions and residual norms match.")
 
 # Test Case 1
 A1 = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]])
@@ -55,46 +56,9 @@ A5 = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
 b5 = np.array([1, 2, 3])
 run_test_case(A5, b5)
 
-# Test Case 6
-A6 = np.array([[[1, 0], [0, 1]]])
-b6 = np.array([2, 3])
+# Test Case 6. Multiple matrix A
+A6 = np.array([[[1, 2], [3, 4], [5, 6]],[[1, 2], [1, 2], [1, 2]]])
+b6 =  np.array([4, 5, 6])
 run_test_case(A6, b6)
 
-# Test Case 7
-A7 = np.array([[[2, 4], [6, 8]]])
-b7 = np.array([5, 10])
-run_test_case(A7, b7)
-
-# Test Case 8
-A8 = np.array([[[1, 1], [0, 0]]])
-b8 = np.array([2, 0])
-run_test_case(A8, b8)
-
-# Test Case 9
-A9 = np.array([[[1, 2, 3], [4, 5, 6]]])
-b9 = np.array([1, 1])
-run_test_case(A9, b9)
-
-# Test Case 10
-A10 = np.array([[[1, 2], [1, 2], [1, 2]]])
-b10 = np.array([1, 2, 3])
-run_test_case(A10, b10)
-
-# Test Case 11. Multiple matrix A, and multiple right-hand side b
-A = np.array([[[1, 2], [3, 4], [5, 6]],[[1, 2], [1, 2], [1, 2]]])
-b_list = [np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9])]
-
-solver = oh.LinearSystemSolver()
-solver.initialize(A)
-
-solutions = []
-
-for b in b_list:
-    x = solver.solve(np.array([[1, 2], [3, 4], [5, 6]]), b)
-    solutions.append(x)
-    print(solver.check_solution(np.array([[1, 2], [3, 4], [5, 6]]), x, b))
-# Print solutions
-for i, x in enumerate(solutions):
-    print(f"Solution for b{i+1}:")
-    print(x)
 
